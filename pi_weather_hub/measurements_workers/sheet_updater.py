5e6877fd4de344e54696b9f1e1e5666fcfc028e2
riosnav <priosnavarrete@gmail.com>
2020-05-24 10:23:15 +0200

    Catch additional exception

--- pi_weather_hub/measurements_workers/sheet_updater.py
+++ pi_weather_hub/measurements_workers/sheet_updater.py
@@ -19,7 +19,7 @@
 # stdlib
 import datetime
 import time
-from socket import timeout
+from socket import timeout, gaierror
 import sys
 
 # pip installed
@@ -90,7 +90,7 @@ class SheetUpdater():
                     values = google_sheets_api_request(SPREADSHEET_ID, RANGE_NAME)
                     self._store_values(values)
                     self._process_stadistics()
-                except (ServerNotFoundError, HttpError, timeout) as e:
+                except (ServerNotFoundError, HttpError, timeout, gaierror) as e:
                     print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S - "),
                           end='', flush=True)
                     print(e, file=sys.stderr, flush=True)
